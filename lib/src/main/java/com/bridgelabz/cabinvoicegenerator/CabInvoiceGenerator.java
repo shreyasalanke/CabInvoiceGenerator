66c756f51c3ee3280593b2aa78ab85e5c06d5447
shreyasalanke <shreyasalanke97@gmail.com>
2021-07-25 22:35:30 +0530

    Initial Commit: Welcome to cab invoice generator

---
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class CabInvoiceGenerator 
+{
+    public static void main(String[] args) 
+     {
+	    System.out.println("Welcome to Cab Invoice Generator");
+     }
+}
